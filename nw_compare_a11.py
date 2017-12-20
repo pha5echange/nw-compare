b5211f5366cd34554e30441e755aa6bee25624ad
pha5echange <jmg@phasechange.co.uk>
2017-12-20 00:36:11 +0000

    3Source262
    3-networks-compared, plus updated plotters

--- nw_compare_a11.py
+++ nw_compare_a11.py
@@ -1,14 +1,15 @@
-# nw_compare_a10.py
-# Version a10
+# nw_compare_a11.py
+# Version a11
 # by jmg - j.gagen*AT*gold*DOT*ac*DOT*uk
-# Sep 29th 2017
+# Nov 16th 2017
 
 # Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
 
 # Makes inclusion lists from master lists and temporally-appropriate GEXFs
 # Generates master dictionary of EN/MB-to-Wiki genre labels from 2015 lists
+# Generates master dictionary of MB-to-Wiki genre labels from 2015 lists
 # Utilises omegaYear input to find GEXFs
-# Uses these to generate echo_inclusion_lists
+# Uses these to generate echo_inclusion_lists and mb_inclusion_lists
 # Then uses dictionary to generate wiki_inclusion_lists
 
 
@@ -28,7 +29,7 @@ from networkx.algorithms.approximation import clique
 from collections import OrderedDict
 from datetime import datetime
 
-versionNumber = ("a10")
+versionNumber = ("a11")
 
 # Initiate timing of run
 runDate = datetime.now()
@@ -68,16 +69,19 @@ if dateIP == 0:
 omegaYear = str(dateIP)
 
 # Open file for writing log
-logPath = os.path.join("logs", omegaYear + '_nw_makelists_' + versionNumber  + '_' + str(runDate) + '_log.txt')
+logPath = os.path.join("logs", omegaYear + '_nw_compare_' + versionNumber  + '_' + str(runDate) + '_log.txt')
 runLog = open(logPath, 'a')
 
 
 runLog.write ("==========================================================================" + '\n' + '\n')
 runLog.write ("Inclusion List Maker | Version " + versionNumber + '\n' + '\n')
 
-# Open files for inclusion_list ouput file
+# Open files for inclusion_list ouput
 echoListPath = os.path.join("inclusion-lists", omegaYear + '_echo_inclusion_list.txt')
 echoListFile = open(echoListPath, 'w')
+
+mbListPath = os.path.join("inclusion-lists", omegaYear + '_mb_inclusion_list.txt')
+mbListFile = open(mbListPath, 'w')
 
 wikiListPath = os.path.join("inclusion-lists", omegaYear + '_wiki_inclusion_list.txt')
 wikiListFile = open(wikiListPath, 'w')
@@ -89,6 +93,9 @@ wikiGexfFile = open(wikiGexfPath, 'w')
 echoGexfPath = os.path.join("gexf", omegaYear + '_nw_compare_echo_' + versionNumber + '.gexf')
 echoGexfFile = open(echoGexfPath, 'w')
 
+mbGexfPath = os.path.join("gexf", omegaYear + '_nw_compare_mb_' + versionNumber + '.gexf')
+mbGexfFile = open(mbGexfPath, 'w')
+
 # Open file for `communities' ouput files
 wikiCommPath = os.path.join("results/communities", omegaYear + '_nw_compare_wiki_' + versionNumber + '_' + str(runDate) + '_communities.txt')
 wikiCommFile = open(wikiCommPath, 'w')
@@ -96,16 +103,22 @@ wikiCommFile = open(wikiCommPath, 'w')
 echoCommPath = os.path.join("results/communities", omegaYear + '_nw_compare_echo_' + versionNumber + '_' + str(runDate) + '_communities.txt')
 echoCommFile = open(echoCommPath, 'w')
 
+mbCommPath = os.path.join("results/communities", omegaYear + '_nw_compare_mb_' + versionNumber + '_' + str(runDate) + '_communities.txt')
+mbCommFile = open(mbCommPath, 'w')
+
 # Open file for analysis results
 anPath = os.path.join("results/analysis", omegaYear + '_nw_compare_' + versionNumber  + '_' + str(runDate) + '_analysis.txt')
 anFile = open(anPath, 'w')
 
 # Open files to write images
 wikiImgPath = os.path.join("networks", omegaYear + '_nw_compare_wiki_' + versionNumber + '_nw.eps')
-wikiImg = open (wikiImgPath, 'w')
+#wikiImg = open (wikiImgPath, 'w')
 
 echoImgPath = os.path.join("networks", omegaYear + '_nw_compare_echo_' + versionNumber + '_nw.eps')
-echoImg = open (echoImgPath, 'w')
+#echoImg = open (echoImgPath, 'w')
+
+mbImgPath = os.path.join("networks", omegaYear + '_nw_compare_mb_' + versionNumber + '_nw.eps')
+#mbImg = open (mbImgPath, 'w')
 
 anFile.write ("==========================================================================" + '\n' + '\n')
 anFile.write ("Inclusion List Maker | Version " + versionNumber + '\n' + '\n')
@@ -115,24 +128,32 @@ anFile.write ("Inclusion List Maker | Version " + versionNumber + '\n' + '\n')
 # Auto-remove self-loops - comment out the line below if self-loop removal is optional
 selfLoopIP = 1
 
-# Create dictionary for en/mb-to-wiki genre mapping
-matchedNodesPath = os.path.join("inclusion-lists", 'echo_wiki_matched_nodes.txt')
+# Create dictionaries for en/mb-to-wiki and mb-to-wiki genre mapping
+matchedNodesPath = os.path.join("inclusion-lists", 'en_wd_mb_matched.txt')
 matchedNodesFile = open(matchedNodesPath, 'r')
 
 print
-print("Storing mapped nodes (Echo:Wiki) in Dictionary...")
+print("Storing mapped nodes (Echo:Wiki and MB:Wiki) in Dictionaries...")
 print
 
-mapDict = {}
+enwdMapDict = {}
+mbwdMapDict = {}
+
 for line in matchedNodesFile:
-	echoNode, wikiNode = line.split(",")
+	echoNode, wikiNode, mbNode = line.split(",")
 	echoNode = echoNode.strip().replace('\n',"")
 	wikiNode = wikiNode.strip().replace('\n',"")
-	mapDict[echoNode] = wikiNode
+	mbNode = mbNode.strip().replace('\n',"")
+	enwdMapDict[echoNode] = wikiNode
+	mbwdMapDict [mbNode] = wikiNode
 	print(str(echoNode) + ',' + str(wikiNode) + '\n')
+	print(str(mbNode) + ',' + str(wikiNode) + '\n')
 
-print("Dictionary complete: ")
-print(mapDict)
+print("Dictionaries complete: ")
+print("Echo:Wiki Dictionary: ")
+print(enwdMapDict)
+print("MB:Wiki Dictionary")
+print(mbwdMapDict)
 
 # Read the EchoNest GEXF and generate graph
 print
@@ -198,7 +219,7 @@ for i in echoNodeList:
 	else:
 		echoNodeOP = str(i).replace(omegaYear + "_","")
 		echoListFile.write(echoNodeOP + '\n')
-		wikiNodeOP = mapDict[echoNodeOP]
+		wikiNodeOP = enwdMapDict[echoNodeOP]
 		wikiListFile.write(wikiNodeOP + '\n')
 
 wikiListFile.close()
@@ -285,6 +306,159 @@ if nx.is_directed(echoNW):
 			echoSinkCount += 1
 else:
 	print("Cannot count sources and sinks. Echo graph is undirected. ")
+	print
+
+# Read the MB GEXF and generate graph
+print
+print ("Reading MB GEXF file and generating graph... ")
+mbInputPath = os.path.join("gexf", omegaYear + '_mb.gexf')
+mbNW = nx.read_gexf(mbInputPath)
+
+# Calculate basic MB graph statistics
+print
+print ('Calculating various things for MB graph...' + '\n')
+mbNodes = nx.number_of_nodes(mbNW)
+mbEdges = nx.number_of_edges(mbNW)
+mbDensity = nx.density(mbNW)
+mbNodeList = nx.nodes(mbNW)
+mbNodeList.sort()
+mbSelfLoopTotal = mbNW.number_of_selfloops()
+mbConnections = mbEdges - mbSelfLoopTotal
+
+print ("Omega Year: " + omegaYear)
+print ('MB Nodes: ' + str(mbNodes))
+print ('MB Edges: ' + str(mbEdges))
+print ('MB Self-loops: ' + str(mbSelfLoopTotal))
+print ('MB Connections (edges minus self-loops): ' + str(mbConnections))
+print ('MB Density: ' + str(mbDensity))
+print
+print (str(nx.info(mbNW)))
+print
+print (str(mbNodeList))
+
+anFile.write ('Network Properties: ' + '\n' + '\n')
+anFile.write ("Omega Year: " + omegaYear + '\n')
+anFile.write ('MB Nodes: ' + str(mbNodes) + '\n')
+anFile.write ('MB Edges: ' + str(mbEdges) + '\n')
+anFile.write ('MB Self-loops: ' + str(mbSelfLoopTotal) + '\n')
+anFile.write ('MB Connections (edges minus self-loops): ' + str(mbConnections) + '\n')
+anFile.write ('MB Density: ' + str(mbDensity) + '\n' + '\n')
+anFile.write (str(nx.info(mbNW)) + '\n' + '\n')
+
+runLog.write ('\n' + 'MB nodes: ' + '\n' + '\n')
+runLog.write (str(mbNodeList) + '\n' + '\n')
+
+# Remove nodes that are not on inclusion-list (`inclusion-lists/mb_inclusion_list.txt')
+
+# Open mb_inclusion_list
+mbRemovedCount = 0
+print ('\n' + 'Checking list and removing MB nodes...' + '\n')
+runLog.write ('Checking list and removing MB nodes...' + '\n' + '\n')
+anFile.write ('Checking list and removing MB nodes...' + '\n' + '\n')
+	
+mbListPath = os.path.join("inclusion-lists", 'master_mb_inclusion_list.txt')
+mbList = open(mbListPath, 'r')
+
+# Check for genres and remove if not present
+mbIncludeNodes = [omegaYear + '_' + line.strip() for line in mbList]
+
+for i in mbNodeList:
+	if not i in mbIncludeNodes:
+		mbNW.remove_node(i) 
+		print ('Removed MB node ' + str(i))
+		runLog.write ('Removed MB node ' + str(i) + '\n')
+		# anFile.write (str(i) + '\n')
+		mbRemovedCount += 1
+	else:
+		mbNodeOP = str(i).replace(omegaYear + "_","")
+		mbListFile.write(mbNodeOP + '\n')
+		#wikiNodeOP = mbwdMapDict[mbNodeOP]
+		#wikiListFile.write(wikiNodeOP + '\n')
+
+#wikiListFile.close()
+
+print ('\n'+ "Removed " + str(mbRemovedCount) + " MB nodes. " + '\n')
+runLog.write ('\n' + "Removed " + str(mbRemovedCount) + " MB nodes. " + '\n' + '\n')
+mbList.close()
+
+# Recalculate basic MB graph statistics
+print ("Recalculating various things for MB graph..." + '\n')
+mbNodes = nx.number_of_nodes(mbNW)
+mbEdges = nx.number_of_edges(mbNW)
+mbDensity = nx.density(mbNW)
+mbNodeList = nx.nodes(mbNW)
+mbNodeList.sort()
+mbSelfLoopTotal = mbNW.number_of_selfloops()
+mbConnections = mbEdges - mbSelfLoopTotal
+
+print ('MB Nodes: ' + str(mbNodes))
+print ('MB Edges: ' + str(mbEdges))
+print ('MB Self-loops: ' + str(mbSelfLoopTotal))
+print ('MB Connections (edges minus self-loops): ' + str(mbConnections))
+print ('MB Density: ' + str(mbDensity))
+print
+print (str(mbNodeList))
+print
+
+anFile.write ('\n' + 'New Network Properties: ' + '\n' + '\n')
+anFile.write ('MB Nodes: ' + str(mbNodes) + '\n')
+anFile.write ('MB Edges: ' + str(mbEdges) + '\n')
+anFile.write ('MB Self-loops: ' + str(mbSelfLoopTotal) + '\n')
+anFile.write ('MB Connections (edges minus self-loops): ' + str(mbConnections) + '\n')
+anFile.write ('MB Density: ' + str(mbDensity) + '\n' + '\n')
+
+runLog.write ('MB nodes: ' + '\n' + '\n')
+runLog.write (str(mbNodeList) + '\n' + '\n')
+
+# Remove MB self-loops
+mbSelfLoopCount = 0
+if selfLoopIP == 1:
+	print ('Checking for and removing MB self-loops...' + '\n')
+	runLog.write('\n' + 'Checking for and removing MB self-loops...' + '\n')
+
+	for u,v in mbNW.edges():
+		if u == v:
+			mbNW.remove_edge(u,v)
+			print ('removed self-loop ' + str(u))
+			mbSelfLoopCount += 1
+
+	if mbSelfLoopCount == 0:
+		print ('No MB self-loops removed.' + '\n')
+		runLog.write('\n' + 'No MB self-loops removed.' + '\n')
+	
+	if mbSelfLoopCount == 1:
+		print ('\n' + 'Removed ' + str(mbSelfLoopCount) + ' MB self-loop edge.')
+		runLog.write ('\n' + 'Removed ' + str(mbSelfLoopCount) + ' MB self-loop edge.' + '\n')
+
+	if mbSelfLoopCount > 1:
+		print ('\n' + 'Removed ' + str(mbSelfLoopCount) + ' MB self-loop edges.')
+		runLog.write ('\n' + 'Removed ' + str(mbSelfLoopCount) + ' MB self-loop edges.' + '\n')
+
+else:
+	print ('No MB self-loops removed.' + '\n')
+	runLog.write('\n' + 'No MB self-loops removed.' + '\n')
+
+# Count MB isolates
+mbIsolateCount = 0
+for i in mbNodeList:
+	if nx.is_isolate(mbNW,i):
+		runLog.write('\n' + 'MB node ' + i + ' is an Isolate. ' + '\n')
+		mbIsolateCount += 1
+
+# Count MB sources and sinks
+mbSourceCount = 0
+mbSinkCount = 0
+if nx.is_directed(mbNW):
+	for i in mbNodeList:
+		mbOutDeg = mbNW.out_degree(i)
+		mbInDeg = mbNW.in_degree(i)
+		if mbOutDeg == 0 and mbInDeg >= 1: 
+			mbSourceCount += 1
+
+		elif mbInDeg == 0 and mbOutDeg >= 1:
+			mbSinkCount += 1
+else:
+	print("Cannot count sources and sinks. MB graph is undirected. ")
 	print
 
 # Read the Wikidata GEXF and generate graph
@@ -464,6 +638,8 @@ nx.write_gexf(wikiNW, wikiGexfFile)
 wikiGexfFile.close()
 nx.write_gexf(echoNW, echoGexfFile)
 echoGexfFile.close()
+nx.write_gexf(mbNW, mbGexfFile)
+mbGexfFile.close()
 
 # Plot and display graphs
 # Graph plotting parameters - moved to config file 'config_nw.txt'
@@ -500,12 +676,15 @@ nx.draw_networkx_edges(wikiNW, graph_pos, width = edge_thickness, alpha = edge_a
 print ('Writing wiki network image file... ' + '\n')
 runLog.write('\n' + "Writing wiki network image file... " + '\n')
 
-plt.savefig(wikiImg, format = 'eps', bbox_inches='tight')
-wikiImg.close()
+plt.savefig(wikiImgPath, format = 'eps', bbox_inches='tight')
+#wikiImg.close()
 
 # display graph
 print ('Displaying wiki graph...' + '\n')
 plt.show()
+
+# Clear plot
+plt.clf()
 
 print ('Laying out echo graph...' + '\n')
 runLog.write('\n' + "Laying out echo graph... " + '\n')
@@ -519,12 +698,37 @@ nx.draw_networkx_edges(echoNW, graph_pos, width = edge_thickness, alpha = edge_a
 print ('Writing echo network image file...' + '\n')
 runLog.write('\n' + "Writing echo network image file... " + '\n')
 
-plt.savefig(echoImg, format = 'eps', bbox_inches='tight')
-echoImg.close()
+plt.savefig(echoImgPath, format = 'eps', bbox_inches='tight')
+#echoImg.close()
 
 # display graph
 print ('Displaying echo graph...' + '\n')
 plt.show()
+
+# Clear plot
+plt.clf()
+
+print ('Laying out MB graph...' + '\n')
+runLog.write('\n' + "Laying out MB graph... " + '\n')
+
+graph_pos = nx.spring_layout(mbNW)
+nx.draw_networkx_nodes(mbNW, graph_pos, node_size = node_size, alpha = node_alpha, node_color=node_colour)
+nx.draw_networkx_edges(mbNW, graph_pos, width = edge_thickness, alpha = edge_alpha, color = edge_colour)
+#nx.draw_networkx_labels(echoNW, graph_pos, font_size = node_text_size, font_family = text_font)
+
+# write image file
+print ('Writing MB network image file...' + '\n')
+runLog.write('\n' + "Writing MB network image file... " + '\n')
+
+plt.savefig(mbImgPath, format = 'eps', bbox_inches='tight')
+#mbImg.close()
+
+# display graph
+print ('Displaying MB graph...' + '\n')
+plt.show()
+
+# Clear plot
+plt.clf()
 
 # Analysis of Wiki graph
 print
@@ -615,7 +819,7 @@ print ('Echo Density: ' + str(echoDensity))
 print ('Echo Isolates: ' + str(echoIsolateCount))
 print
 
-anFile.write ('Final Network Properties: ' + '\n' + '\n')
+anFile.write ('\n' + 'Final Network Properties: ' + '\n' + '\n')
 anFile.write ('Echo Nodes: ' + str(echoNodes) + '\n')
 anFile.write ('Echo Edges: ' + str(echoEdges) + '\n')
 anFile.write ('Echo Self-loops: ' + str(echoSelfLoopTotal) + '\n')
@@ -680,10 +884,99 @@ except:
 	pass
 
 anFile.write ('\n' + 'Confirmation: ' + '\n' + str(nx.info(echoNW)))
-anFile.close()
 
 runLog.write('\n' + "Echo graph is " + echoIsDigraph + '\n')
 runLog.write ('\n' + "Properties: " + '\n' + str(nx.info(echoNW)) + '\n')
+
+# Recalculate basic MB graph statistics
+print ('Analysing MB graph...' + '\n')
+mbNodes = nx.number_of_nodes(mbNW)
+mbEdges = nx.number_of_edges(mbNW)
+mbDensity = nx.density(mbNW)
+mbNodeList = nx.nodes(mbNW)
+mbNodeList.sort()
+mbSelfLoopTotal = mbNW.number_of_selfloops()
+mbConnections = mbEdges - mbSelfLoopTotal
+mbDegreeSeq = sorted(nx.degree(mbNW).values(),reverse=True)
+mbMaxDeg = max(mbDegreeSeq)
+
+print ('MB Nodes: ' + str(mbNodes))
+print ('MB Edges: ' + str(mbEdges))
+print ('MB Self-loops: ' + str(mbSelfLoopTotal))
+print ('MB Connections (edges minus self-loops): ' + str(mbConnections))
+print ('MB Density: ' + str(mbDensity))
+print ('MB Isolates: ' + str(mbIsolateCount))
+print
+
+anFile.write ('\n' + 'Final Network Properties: ' + '\n' + '\n')
+anFile.write ('MB Nodes: ' + str(mbNodes) + '\n')
+anFile.write ('MB Edges: ' + str(mbEdges) + '\n')
+anFile.write ('MB Self-loops: ' + str(mbSelfLoopTotal) + '\n')
+anFile.write ('MB Connections (edges minus self-loops): ' + str(mbConnections) + '\n')
+anFile.write ('MB Density: ' + str(mbDensity) + '\n')
+anFile.write ('MB Isolates: ' + str(mbIsolateCount) + '\n' + '\n')
+
+# Calculate modularity/community memberships
+try:
+	if nx.is_directed(mbNW):
+		mbAvClustering = 0
+		mbConnectComp = 0
+		mbIsDigraph = ("Directed")
+		mbUndNW = mbNW.to_undirected()
+		mbPart = community.best_partition(mbUndNW)
+		mbMod = community.modularity(mbPart, mbUndNW)
+		mbAvClustering = nx.average_clustering(mbUndNW)
+		mbConnectComp = [len(c) for c in sorted(nx.connected_components(mbUndNW), key=len, reverse=True)]
+	else:
+		mbPart = community.best_partition(mbNW)
+		mbMod = community.modularity(mbPart, mbNW)
+		mbAvClustering = nx.average_clustering(mbNW)
+		mbConnectComp = [len(c) for c in sorted(nx.connected_components(mbNW), key=len, reverse=True)]
+		mbIsDigraph = ("Undirected")
+except:
+	mbMod = 0
+	mbAvClustering = 0
+	mbConnectComp = 0
+
+print ("MB graph is " + mbIsDigraph)
+print ('MB Nodes: ' + str(mbNodes))
+print ('MB Isolates: ' + str(mbIsolateCount))
+print ('MB Edges: ' + str(mbEdges))
+print ('MB Density: ' + str(mbDensity))
+print ('MB Maximal degree: ' + str(mbMaxDeg))
+print ("MB modularity: " + str(mbMod))
+print ('MB Average Clustering Coefficient: ' + str(mbAvClustering))
+print ('MB Connected Components: ' + str(mbConnectComp))
+print ("Source nodes: " + str(mbSourceCount))
+print ("Sink nodes: " + str(mbSinkCount))
+print
+
+anFile.write ('\n' + 'MB Network Properties: ' + '\n' + '\n')
+anFile.write ("Omega Year: " + omegaYear + '\n')
+anFile.write("MB graph is " + mbIsDigraph + '\n')
+anFile.write ('MB Nodes: ' + str(mbNodes) + '\n')
+anFile.write ('MB Isolates: ' + str(mbIsolateCount) + '\n')
+anFile.write ('MB Edges: ' + str(mbEdges) + '\n')
+anFile.write ('MB Density: ' + str(mbDensity) + '\n')
+anFile.write ('MB Maximal degree: ' + str(mbMaxDeg) + '\n')
+anFile.write ("MB modularity: " + str(mbMod) + '\n')
+anFile.write ('MB Average Clustering Coefficient: ' + str(mbAvClustering) + '\n')
+anFile.write ('MB Connected Components: ' + str(mbConnectComp) + '\n')
+anFile.write ("Source nodes: " + str(mbSourceCount) + '\n')
+anFile.write ("Sink nodes: " + str(mbSinkCount) + '\n' + '\n')
+
+try:
+	for key,value in mbPart.items():
+		genLabel = key.replace("2015_", omegaYear + '_')
+		mbCommFile.write(genLabel + "," + str(value) + '\n')
+except:
+	pass
+
+anFile.write ('\n' + 'Confirmation: ' + '\n' + str(nx.info(mbNW)))
+anFile.close()
+
+runLog.write('\n' + "MB graph is " + mbIsDigraph + '\n')
+runLog.write ('\n' + "Properties: " + '\n' + str(nx.info(mbNW)) + '\n')
 
 # End timing of run
 endTime = datetime.now()
